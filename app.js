ddcd4c13ed2521da291bf3885b993892195c060a
joukey123 <0162943362@naver.com>
2023-10-24 01:08:01 +0900

    add mediaQuery max950

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const menu = document.querySelector("#nav > ul");
+const nav = document.getElementById("nav");
+const menu = document.querySelector(".nav__menu");
 const subMenus = document.querySelectorAll(".subMenu");
 const navBg = document.querySelector(".nav_bg");
 const header = document.querySelector("header");
 const reserveBtn = document.querySelector(".reserveBtn");
-
+const box = document.getElementById("box");
+const lis = menu.querySelectorAll("li");
+const closeBtn = document.querySelector(".closeBtn");
 const handleHideMenu = () => {
   for (const subMenu of subMenus) {
     subMenu.style.visibility = "hiddne";
@@ -22,6 +25,74 @@ const handleShowMenu = () => {
   navBg.style.opacity = 1;
 };
 
-menu.addEventListener("mouseover", handleShowMenu);
-header.addEventListener("mouseleave", handleHideMenu);
+const mql = window.matchMedia("(max-width: 950px)");
+
+function handleHideMobileMenu(ul) {
+  ul.id = "";
+  ul.style.visibility = "hidden";
+  ul.style.animation = "slideup .1s linear forwards";
+}
+
+function handleShowMobileMenu() {
+  const ul = this.querySelector("ul");
+  if (ul.id) {
+    handleHideMobileMenu(ul);
+  } else {
+    ul.style.visibility = "visible";
+    ul.style.animation = "slidedown .3s linear forwards";
+    ul.id = "clicked";
+  }
+}
+
+const handleSlideMenu = () => {
+  menu.style.animation = "rightSlide .3s linear forwards";
+  box.classList.remove("hidden");
+  for (let li of lis) {
+    li.addEventListener("click", handleShowMobileMenu);
+  }
+};
+const handleCloseSlideMenu = () => {
+  menu.style.animation = "leftSlide .3s linear forwards";
+  box.classList.add("hidden");
+};
+
+const hadleMobileScreen = () => {
+  const ul = document.createElement("ul");
+  const li = document.createElement("li");
+  const i = document.createElement("i");
+  const logo = document.querySelector(".logo > a");
+
+  if (mql.matches) {
+    ul.className = "menuBtn";
+    i.className = "fa-solid fa-bars";
+    li.appendChild(i);
+    ul.appendChild(li);
+    nav.appendChild(ul);
+    logo.style.background =
+      "url('../src/img/mlogo.png') 50% 50% / 3rem no-repeat";
+    closeBtn.classList.remove("hidden");
+    menu.style.animation = "";
+    box.classList.add("hidden");
+    menu.querySelector(".reserveBtn").classList.remove("hidden");
+    i.addEventListener("click", handleSlideMenu);
+    menu.removeEventListener("mouseover", handleShowMenu);
+    header.removeEventListener("mouseleave", handleHideMenu);
+  } else {
+    const menuUl = document.querySelector(".menuBtn");
+    if (menuUl) {
+      menuUl.remove();
+      logo.style.background = "";
+    }
+    menu.style.animation = "";
+    menu.addEventListener("mouseover", handleShowMenu);
+    header.addEventListener("mouseleave", handleHideMenu);
+    closeBtn.classList.add("hidden");
+    menu.querySelector(".reserveBtn").classList.add("hidden");
+  }
+};
+
+hadleMobileScreen();
+
 reserveBtn.addEventListener("mouseover", handleHideMenu);
+mql.addEventListener("change", hadleMobileScreen);
+closeBtn.addEventListener("click", handleCloseSlideMenu);
