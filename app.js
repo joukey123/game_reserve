6638fc19807e64e7b9497b18b4f16ff9490f3b1a
joukey123 <0162943362@naver.com>
2023-10-28 00:03:22 +0900

    input only number

--- app.js
+++ app.js
@@ -17,14 +17,15 @@ const timerSpan = document.createElement("span");
 const accessNumber = document.querySelector(".access__number");
 const cards = document.querySelectorAll(".dungeon__bg");
 const submit = document.querySelector("input[value='예약하기']");
-const asd = () => {
-  if (adAccess.checked && infoAccess.checked && allAccess.checked) {
-    console.log("확인");
-  } else {
-    console.log("동의해주세요");
-  }
-};
-submit.addEventListener("click", asd);
+const inputNumber = document.querySelector("input[name=phoneNumber]");
+
+const inputToken = document.querySelector("input[name=access_token]");
+const contents = document.querySelectorAll(".dungeon__contents-img");
+const accessTokenConfirm = document.querySelector(
+  ".access__number input[type='button']"
+);
+const reserveForm = document.querySelector(".reserveform");
+
 const mql = window.matchMedia("(max-width: 950px)");
 
 let isClicked = true; //allAgree
@@ -165,52 +166,81 @@ let time = 60;
 let min = 0;
 let sec = 0;
 let intervalId;
-const accessTokenConfirm = document.querySelector(
-  ".access__number input[type='button']"
-);
-const reserveForm = document.querySelector(".reserveform");
 
 const deleteErrorMessage = () => {
   reserveForm.querySelector(".message").remove();
-  accessTokenConfirm.addEventListener("click", handleAccessToken);
+  submit.addEventListener("click", handleReserveBtn);
+  if (seuccessToken) {
+    accessTokenConfirm.removeEventListener("click", handleAccessToken);
+  } else {
+    accessTokenConfirm.addEventListener("click", handleAccessToken);
+  }
 };
 
+let seuccessToken = true;
+const createMessage = () => {
+  const messageBox = document.createElement("span");
+  reserveForm.appendChild(messageBox);
+  messageBox.className = "message";
+  return (message = document.querySelector(".message"));
+};
 const handleAccessToken = () => {
-  const message = document.createElement("span");
-  reserveForm.appendChild(message);
-  message.className = "message";
-
-  const inputToken = document.querySelector("input[name=access_token]");
+  createMessage();
   if (inputToken.value) {
     clearInterval(intervalId);
-    timerSpan.innerText = "인증완료";
     time = 60;
+    timerSpan.innerText = "";
     message.classList.add("ok");
     message.innerText = "인증을 완료했습니다.";
+    accessTokenConfirm.style.backgroundColor = "teal";
+    accessTokenConfirm.style.color = "var(--font-color)";
+    accessTokenConfirm.value = "인증완료";
+    setTimeout(deleteErrorMessage, 1000);
     accessTokenConfirm.removeEventListener("click", handleAccessToken);
-    setTimeout(deleteErrorMessage, 2000);
     phoneSendBtn.addEventListener("click", handleSendPhone);
+    seuccessToken = true;
+    inputToken.value = "";
   } else {
+    timerSpan.innerText = "";
     message.innerText = "인증번호를 입력해주세요.";
     accessTokenConfirm.removeEventListener("click", handleAccessToken);
-    setTimeout(deleteErrorMessage, 2000);
+    setTimeout(deleteErrorMessage, 1000);
+    seuccessToken = false;
   }
 };
 const handleSendPhone = () => {
-  accessTokenConfirm.style.backgroundColor = "var(--yellow)";
-  accessTokenConfirm.addEventListener("click", handleAccessToken);
-  handleTimer();
+  if (inputNumber.value) {
+    accessTokenConfirm.style.backgroundColor = "var(--yellow)";
+    accessTokenConfirm.addEventListener("click", handleAccessToken);
+    accessTokenConfirm.value = "인증번호 확인";
+    inputToken.value = "";
+    accessTokenConfirm.style.color = "black";
+    seuccessToken = false;
+    if (intervalId) {
+      clearInterval(intervalId);
+      time = 60;
+      handleTimer();
+    } else {
+      handleTimer();
+    }
+  } else {
+    timerSpan.innerText = "";
+    createMessage();
+    message.innerText = "전화번호를 입력해주세요.";
+    setTimeout(deleteErrorMessage, 1000);
+    clearInterval(intervalId);
+    time = 60;
+  }
 };
 
 //3분 타이머 함수
 const timer = () => {
   min = String(Math.floor(time / 60)).padStart(2, 0);
   sec = String(time % 60).padStart(2, 0);
-
   timerSpan.innerText = `${min} : ${sec}`;
   timerSpan.className = "timer";
   accessNumber.appendChild(timerSpan);
-  phoneSendBtn.removeEventListener("click", handleSendPhone);
+  // phoneSendBtn.removeEventListener("click", handleSendPhone);
   if (time < 0) {
     clearInterval(intervalId);
     timerSpan.innerText = "시간초과";
@@ -248,13 +278,78 @@ function handleCard() {
   document.body.appendChild(bigImgBox);
 }
 
-hadleMobileScreen();
+const handleReserveBtn = () => {
+  if (
+    adAccess.checked &&
+    infoAccess.checked &&
+    allAccess.checked &&
+    accessTokenConfirm.value === "인증완료"
+  ) {
+    const popUpBox = document.createElement("div");
 
-const contents = document.querySelectorAll(".dungeon__contents-img");
+    const reserveSuccessBox = document.createElement("div");
+    const succesBoxImg = document.createElement("img");
+    const textBox = document.createElement("div");
+    const downloadBox = document.createElement("div");
+    const aosImg = document.createElement("div");
+    const iosImg = document.createElement("div");
+    const btn = document.createElement("input");
+    const h1 = document.createElement("h1");
+    const p = document.createElement("p");
+    popUpBox.className = "popup__box";
+    reserveSuccessBox.className = "success__box";
+    // succesBoxImg.src = "/src/img/su1.png";
+    btn.type = "button";
+    btn.innerText = "완료";
+    h1.innerText = "아제로스에서 다양한 모험을 경험하세요.";
+    p.innerText = "예약해주셔서 감사합니다!";
+
+    document.body.appendChild(popUpBox);
+    popUpBox.appendChild(reserveSuccessBox);
+    reserveSuccessBox.appendChild(succesBoxImg);
+    reserveSuccessBox.appendChild(textBox);
+    textBox.appendChild(h1);
+    textBox.appendChild(p);
+    reserveSuccessBox.appendChild(downloadBox);
+    downloadBox.appendChild(aosImg);
+    downloadBox.appendChild(iosImg);
+
+    reserveSuccessBox.appendChild(btn);
+  } else if (accessTokenConfirm.value === "인증번호 확인") {
+    createMessage();
+    message.innerText = "휴대전화 인증을 진행해주세요.";
+    submit.removeEventListener("click", handleReserveBtn);
+    setTimeout(deleteErrorMessage, 1000);
+    seuccessToken = false;
+  } else {
+    createMessage();
+    message.innerText = "필수사항에 동의해주세요.";
+    submit.removeEventListener("click", handleReserveBtn);
+    setTimeout(deleteErrorMessage, 1000);
+    seuccessToken = false;
+  }
+};
+
+const checkedNumber = (event) => {
+  const text = event.target.value;
+  const regex = /[^0-9]/g;
+
+  if (regex.test(text)) {
+    event.target.value = text.replace(regex, "");
+    createMessage();
+    message.innerText = "숫자만 입력해주세요.";
+    setTimeout(deleteErrorMessage, 1000);
+  }
+};
+hadleMobileScreen();
 
 for (let card of cards) {
   card.addEventListener("click", handleCard);
 }
+
+inputNumber.addEventListener("input", checkedNumber);
+inputToken.addEventListener("input", checkedNumber);
+submit.addEventListener("click", handleReserveBtn);
 phoneSendBtn.addEventListener("click", handleSendPhone);
 adAccess.addEventListener("click", handleClickadAccess);
 infoAccess.addEventListener("click", handleClickInfoAccess);
